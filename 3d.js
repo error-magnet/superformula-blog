e2652be26ad3e734f5f4354508ccc851b9f1e84f
Chaitanya Hegde <chaitanya.hegde@atherenergy.com>
2016-08-18 16:01:39 +0530

    scaled the plots, made it more stable

--- 3d.js
+++ 3d.js
@@ -1,123 +1,169 @@
+//Function to create 3D superformula plot
+
 var ThreeD = function(){
     var _this = this;
-    this.plot3D = function(obj){
-        _this.clear();
+    this.plot3D = function(state){
 
-        var div = document.getElementById('root');
+        //if there is an existing scene, just update the plot
+        if(_this.scene){
+            _this.updateGeom(state); 
+        }
+        else{
+            //setup a THREE JS scene
+            var div = document.getElementById('root');
+            _this.renderer = new THREE.WebGLRenderer({ antialias: true });
+            _this.renderer.setClearColor(0xffffff);
+            _this.renderer.setPixelRatio(window.devicePixelRatio);
+            _this.renderer.setSize(window.innerWidth, window.innerHeight);
+            div.appendChild(_this.renderer.domElement);
+            _this.scene = new THREE.Scene();
 
-        _this.renderer = new THREE.WebGLRenderer();
-        _this.renderer.setClearColor(0xffffff);
-        _this.renderer.setPixelRatio(window.devicePixelRatio);
-        _this.renderer.setSize(window.innerWidth, window.innerHeight);
-        div.appendChild(_this.renderer.domElement);
-        _this.scene = new THREE.Scene();
-        _this.camera = new THREE.PerspectiveCamera( 
-                45, window.innerWidth / window.innerHeight, 1, 1000 );
-        _this.camera.position.set( 0, 0, 300 );
-        _this.controls = new THREE.TrackballControls( 
-                _this.camera, _this.renderer.domElement);
-        _this.controls.minDistance = 0.01;
-        _this.controls.maxDistance = 500;
-        _this.controls.noZoom = false;
-        _this.controls.noPan = false;
-        _this.scene.add(new THREE.AmbientLight(0x222222));
-        var light = new THREE.PointLight(0xffffff);
-        light.position.copy(_this.camera.position);
-        _this.scene.add(light);
+            //setup camera
+            _this.camera = new THREE.PerspectiveCamera( 
+                    45, window.innerWidth / window.innerHeight, 1, 1000 );
+            _this.camera.position.set(0, 0, 300);
+            _this.controls = new THREE.TrackballControls( 
+                    _this.camera, _this.renderer.domElement);
+            _this.controls.minDistance = 0.01;
+            _this.controls.maxDistance = 500;
+            _this.controls.noZoom = false;
+            _this.controls.noPan = false;
 
+            /*
+            var axisHelper = new THREE.AxisHelper(50);
+            _this.scene.add(axisHelper);
+            */
 
-        var axisHelper = new THREE.AxisHelper(50);
-        _this.scene.add(axisHelper);
-        var geom;
-    /*    var m = 4, n1 = 10, n2 = 10, n3 = 10, a = 2, b = 2;
-        var M = 4, N1 = 10, N2 = 10, N3 = 10, A = 2, B = 2;   
-    */
-        var m = obj.m, n1 = obj.n1, n2 = obj.n2, n3 = obj.n3, a = obj.a, b = obj.b;
-        var M = obj.M, N1 = obj.N1, N2 = obj.N2, N3 = obj.N3, A = obj.A, B = obj.B;
-        geom = new THREE.Geometry();
+            //create line with superformula geometry
+            _this.updateGeom(state);
 
-        for(var i=0; i<=360; i=i+1){
-            for(var j=0; j<=360; j=j+1){
+     
+            //add it to the scene
+            _this.scene.add(_this.line);
+
+            //animate the scene, mainly for zooming, panning etc
+            //if the parameters are changed for 3D, vertices are updated
+            var animate = function(){
+                _this.line.geometry.verticesNeedUpdate = true;
+                _this.controls.update();
+                _this.renderer.render(_this.scene, _this.camera);
+                _this.id = requestAnimationFrame(animate);
+            };
+
+            animate();
+        }
+    };
+
+    //clear the scene when not in use, else it will harm the performance
+    this.clear = function(){
+        if(_this.renderer){
+            //stop the animation
+            cancelAnimationFrame(this.id);
+            //remove listener to render
+            _this.renderer.domElement.addEventListener('dblclick', null, false);
+            _this.scene = null;
+            _this.projector = null;
+            _this.camera = null;
+            _this.controls = null;
+
+            //remove the canvas entirely
+            try{
+                document.querySelector('canvas').remove();    
+            }
+            catch(e){};
+        }
+        
+    };
+
+    //function to calculate the coordinates from the superformula
+    //and create the geometry
+    this.updateGeom = function(state){
+        var data = [], xMin = Infinity, xMax = -Infinity, 
+            yMin = Infinity, yMax = -Infinity,
+            zMin = Infinity, zMax = -Infinity;
+    
+        var m1 = state.m1, m2 = state.m2, n1 = state.n1, n2 = state.n2, 
+            n3 = state.n3, a = state.a, b = state.b;
+        var M1 = state.M2, M2 = state.M2, N1 = state.N1, N2 = state.N2, 
+            N3 = state.N3, A = state.A, B = state.B;
+
+        //Superformula by John Gielles
+        //Extended to 3D supershape
+        //Equations from: http://paulbourke.net/geometry/supershape/
+        for(var i=-180; i<=180; i=i+5){
+            for(var j=-90; j<=90; j=j+5){
+
+                //to radians
                 var theta = i*Math.PI/180;
                 var phi = j*Math.PI/180;
 
-                //Superformula by John Gielles
-                //Extended to 3D supershape
-                //Equations from: http://paulbourke.net/geometry/supershape/
-
-                var term11 = Math.pow(Math.abs((1/a)*Math.cos(theta*m/4)), n2);
-                var term12 = Math.pow(Math.abs((1/b)*Math.sin(theta*m/4)), n3);
-                var denom1 = Math.pow(term11 + term12, 1/n1);
+                var term11 = Math.pow(Math.abs((1/a)*Math.cos(theta*m1/4)), n2);
+                var term12 = Math.pow(Math.abs((1/b)*Math.sin(theta*m2/4)), n3);
+                var denom1 = Math.pow(term11 + term12, -1/n1);
                 var r1 =  1/denom1;
                 
-                var term21 = Math.pow(Math.abs((1/A)*Math.cos(phi*M/4)), N2);
-                var term22 = Math.pow(Math.abs((1/B)*Math.sin(phi*M/4)), N3);
-                var denom2 = Math.pow(term21 + term22, 1/N1);
+
+                var term21 = Math.pow(Math.abs((1/A)*Math.cos(phi*M1/4)), N2);
+                var term22 = Math.pow(Math.abs((1/B)*Math.sin(phi*M2/4)), N3);
+                var denom2 = Math.pow(term21 + term22, -1/N1);
                 var r2 =  1/denom2;
                 
                 var x = 3*r1*Math.cos(theta)*r2*Math.cos(phi);
                 var y = 3*r1*Math.sin(theta)*r2*Math.cos(phi);
                 var z = 3*r2*Math.sin(phi);
-                var v = new THREE.Vector3(x, y, z);
-                
 
-                geom.vertices.push(v);
-                var face1 = new THREE.Face3((i)*360+j, (i+1)*360+j,  (i)*360+j+1);
-                //face1.color.setRGB(0.3, 0.3, 0.3);
-                face1.color.setRGB((j)/500, (j)/500, (j)/500);
-                var face2 = new THREE.Face3((i+1)*360+j, (i)*360+j, (i+1)*360+j-1);
-                //face2.color.setRGB(0.3, 0.3, 0.3);
-                face2.color.setRGB((j)/500, (j)/500, (j)/500);
-                geom.faces.push(face1);
-                geom.faces.push(face2);
+                data.push({x: x, y: y, z: z});
 
-    /*          //just plot veritices for reference
-                var dotMaterial = new THREE.PointsMaterial({size: 2, sizeAttenuation: false, color: 0xffffff});
-                var dotGeometry = new THREE.Geometry();
-                dotGeometry.vertices.push(new THREE.Vector3(x, y, z));
-                var dot = new THREE.Points(dotGeometry, dotMaterial);
-                scene.add(dot);
-    */
+                //calculate min max to we can scale it
+                xMin = xMin > x ? x : xMin;
+                xMax = xMax < x ? x : xMax;
+                yMin = yMin > y ? y : yMin;
+                yMax = yMax < y ? y : yMax;
+                zMin = zMin > z ? z : zMin;
+                zMax = zMax < z ? z : zMax;
             }
         }
 
-        var material = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors})
-        _this.object = new THREE.Mesh(geom, material);
-        _this.scene.add(_this.object);
-        _this.renderer.render(_this.scene, _this.camera);
+        //scale it so the plot fits nicely on the screen
+        var xScale = d3.scale.linear().range([-50, 50]).domain([xMin, xMax]);
+        var yScale = d3.scale.linear().range([-50, 50]).domain([yMax, yMin]);
+        var zScale = d3.scale.linear().range([-50, 50]).domain([zMin, zMax]);
+        var points = [];       
+
+        //create points vector
+        data.forEach(function(d){
+            points.push(new THREE.Vector3(xScale(d.x), 
+                    yScale(d.y), zScale(d.z)));        
+        })        
+        
+        //create a spline so we get a smooth curve
+        var spline = new THREE.CatmullRomCurve3(points);
+    
+        //get the spline points
+        var splinePoints = spline.getPoints(data.length);
 
 
-        var animate = function(){
-            _this.controls.update();
-            _this.renderer.render(_this.scene, _this.camera);
-            _this.id = requestAnimationFrame(animate);
-        };
-
-        animate();
-    };
-    this.empty = function(elem) {
-            while (elem.lastChild) elem.removeChild(elem.lastChild);
-        };
-
-    this.clear = function(){
-        if(_this.renderer){
-            cancelAnimationFrame(this.id);// Stop the animation
-            _this.renderer.domElement.addEventListener('dblclick', null, false); //remove listener to render
-            _this.scene = null;
-            _this.projector = null;
-            _this.camera = null;
-            _this.controls = null;
-            _this.empty(_this.object);    
+        if(!_this.geometry)
+            _this.geometry = new THREE.Geometry();
+        
+        //create the geometry based on the spline points
+        //and color it
+        for(var i = 0; i < splinePoints.length; i++){
+            _this.geometry.vertices[i] = splinePoints[i];  
+            _this.geometry.colors[ i ] = new THREE.Color(0.3, 0.3, 0.3);
+            _this.geometry.colors[ i + 1 ] = _this.geometry.colors[ i ];
         }
         
+        var material = new THREE.LineBasicMaterial({
+                linewidth: 3,
+                vertexColors: THREE.VertexColors
+            });
+
+        //create the new line
+       _this.line = new THREE.Line(_this.geometry, material);
+    
     };
 
 };
-
-
-/*
-
-
-*/
 
 
--- main.js
+++ main.js
@@ -1,111 +1,150 @@
-
+/*********
+ * Superformula explorer in 2D and 3D
+ *
+ * Libraries used: d3.js, three.js, dat.gui
+ *
+ * *********/
 
 var gui = new dat.GUI();
-var init = {type: '3D', m: 1, n1:4, n2:4, n3: 3, a:10, b:10, M:1, N1:3, N2:3, N3:3, A:5, B:5};
+var state = {type: '3D', m1: 20, m2: 8, n1:1, n2:1, n3: 8, a:10, b:10, M1:1, 
+    M2:10, N1:3, N2:3, N3:3, A:5, B:5};
 var root = document.getElementById('root');
 
 var threed = new ThreeD();
+var twod = new TwoD();
 
-gui.add(init, 'type', ['2D', '3D'])
+//Add control panel to change parameters
+
+// Dropdown for 2D/3D
+gui.add(state, 'type', ['2D', '3D'])
     .onFinishChange(function(val){
-        init.type = val;
-        plot(init);
+        state.type = val;
+        plot(state);
     });
 
+//2D control panel
 var f2d = gui.addFolder('Parameters - (both 2D & 3D)')
-f2d.add(init, 'm', -10, 10).listen()
+f2d.add(state, 'm1', -10, 1000)
     .onFinishChange(function(val){
-        init.m = val;
-        plot(init);
+        state.m1 = val;
+        plot(state);
     });
-f2d.add(init, 'n1', -10, 10).listen()
+f2d.add(state, 'm2', -10, 1000)
     .onFinishChange(function(val){
-        init.n1 = val;
-        plot(init);
+        state.m2 = val;
+        plot(state);
     });
-f2d.add(init, 'n2', -10, 10).listen()
+f2d.add(state, 'n1', -10, 1000)
     .onFinishChange(function(val){
-        init.n2 = val;
-        plot(init);
+        state.n1 = val;
+        plot(state);
     });
-f2d.add(init, 'n3', -10, 10).listen()
+f2d.add(state, 'n2', -10, 1000)
     .onFinishChange(function(val){
-        init.n3 = val;
-        plot(init);
+        state.n2 = val;
+        plot(state);
     });
-f2d.add(init, 'a', -10, 10).listen()
+f2d.add(state, 'n3', -10, 1000)
     .onFinishChange(function(val){
-        init.a = val;
-        plot(init);
+        state.n3 = val;
+        plot(state);
     });
-f2d.add(init, 'b', -10, 10).listen()
+f2d.add(state, 'a', -10, 1000)
     .onFinishChange(function(val){
-        init.b = val;
-        plot(init);
+        state.a = val;
+        plot(state);
+    });
+f2d.add(state, 'b', -10, 1000)
+    .onFinishChange(function(val){
+        state.b = val;
+        plot(state);
     });
 
+//3D control panel
 var f3d = gui.addFolder('Additional parameters for 3D')
-f3d.add(init, 'M', -10, 10).listen()
+f3d.add(state, 'M1', -10, 1000)
     .onFinishChange(function(val){
-        init.M = val;
-        plot(init);
+        state.M1 = val;
+        plot(state);
     });
-f3d.add(init, 'N1', -10, 10).listen()
+f3d.add(state, 'M2', -10, 1000)
     .onFinishChange(function(val){
-        init.N1 = val;
-        plot(init);
+        state.M2 = val;
+        plot(state);
     });
-f3d.add(init, 'N2', -10, 10).listen()
+f3d.add(state, 'N1', -10, 1000)
     .onFinishChange(function(val){
-        init.N2 = val;
-        plot(init);
+        state.N1 = val;
+        plot(state);
     });
-f3d.add(init, 'N3', -10, 10).listen()
+f3d.add(state, 'N2', -10, 1000)
     .onFinishChange(function(val){
-        init.N3 = val;
-        plot(init);
+        state.N2 = val;
+        plot(state);
     });
-f3d.add(init, 'A', -10, 10).listen()
+f3d.add(state, 'N3', -10, 1000)
     .onFinishChange(function(val){
-        init.A = val;
-        plot(init);
+        state.N3 = val;
+        plot(state);
     });
-f3d.add(init, 'B', -10, 10).listen();
+f3d.add(state, 'A', -10, 1000)
+    .onFinishChange(function(val){
+        state.A = val;
+        plot(state);
+    });
+f3d.add(state, 'B', -10, 1000)
 
 f2d.open();
 f3d.open();
 
+//select random values
 var random = function(){
-    init.m = 10*Math.random();
-    init.n1 = 10*Math.random();
-    init.n2 = 10*Math.random();
-    init.n3 = 10*Math.random();
-    init.a = 10*Math.random();
-    init.b = 10*Math.random();
-    init.M = 10*Math.random();
-    init.N1 = 10*Math.random();
-    init.N2 = 10*Math.random();
-    init.N3 = 10*Math.random();
-    init.A = 10*Math.random();
-    init.B = 10*Math.random();
-        
-  
-    plot(init);
+    state.m = 200*Math.random();
+    state.n1 = 200*Math.random();
+    state.n2 = 200*Math.random();
+    state.n3 = 200*Math.random();
+    state.a = 200*Math.random();
+    state.b = 200*Math.random();
+    state.M = 200*Math.random();
+    state.N1 = 200*Math.random();
+    state.N2 = 200*Math.random();
+    state.N3 = 200*Math.random();
+    state.A = 200*Math.random();
+    state.B = 200*Math.random();
+    
+    //update dat gui to reflect the randomize changes
+    //.listen() could be used, but it is bugged
+    //so manually updating each controller value
+    for(var f in gui.__folders){
+        gui.__folders[f].__controllers.forEach(function(c){
+            c.updateDisplay();
+        });
+    }
+     
+    plot(state);
 };
 
-
+//add a randomize function for fun
 gui.add(window, 'random').onFinishChange(function(){
-       plot(init);
+       plot(state);
     });
 
-
-var plot = function(init){
-    root.innerHTML = '';
-    if(init.type === '2D')
-        plot2D(init);
-    else
-        threed.plot3D(init);
+var plot = function(state){
+    if(state.type === '2D'){
+        //clear 3D plot if it exists
+        threed.clear();
+        //create/update 2D plot
+        twod.update(state);
+    }
+    else{
+        //remove 2D plot
+        d3.select('svg').remove();
+        //create/update 3D plot
+        threed.plot3D(state);
+    }
 
 };
 
-plot(init);
+//actually plot the formula!
+plot(state);
+
